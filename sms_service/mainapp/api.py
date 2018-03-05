cf848d87aa28551853a3bd760347310f167b01ec
Govinda Gupta <govinda.gupta@gmail.com>
2018-03-05 16:06:26 +0530

    outbound api first cut

--- sms_service/mainapp/api.py
+++ sms_service/mainapp/api.py
@@ -17,6 +17,8 @@ class InboundSMS(View):
                                 {'param': 'text', 'required': True, 'regex': '^.{1,120}$'}]
 
             do_input_validation(data, json_input_rules)
+
+            # TODO -  Authentication against account
             PhoneNumber.objects.get(number=data['to'])
 
             #everything good, process the message
@@ -38,5 +40,30 @@ class InboundSMS(View):
 class OutboundSMS(View):
 
     def post(self, request):
-        ret = {'message': '', 'error': '<parameter_name> is missing'}
+
+        ret = {'message': '', 'error': ''}
+        try:
+            data = json.loads(request.body.decode('utf-8'))
+            json_input_rules = [{'param': 'from', 'required': True, 'regex': '^.{6,16}$'},
+                                {'param': 'to', 'required': True, 'regex': '^.{6,16}$'},
+                                {'param': 'text', 'required': True, 'regex': '^.{1,120}$'}]
+
+            do_input_validation(data, json_input_rules)
+
+            #TODO -  Authentication against account
+            PhoneNumber.objects.get(number=data['from'])
+
+            #everything good, process the message
+            ret['message'] = 'inbound sms ok'
+
+        except ValidationError as e:
+            print("ValidationError", e.message)
+            ret['error'] = e.message
+        except PhoneNumber.DoesNotExist:
+            print('to number not exist')
+            ret['error'] = 'to number not exist'
+        except:
+            print ('unknown failure')
+            ret['error'] = 'unknown failure'
+
         return JsonResponse(ret)
