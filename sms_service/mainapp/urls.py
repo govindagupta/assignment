1c4e4f6b31e28a63b8e18c04a1a9d34e50a78ee3
Govinda Gupta <govinda.gupta@gmail.com>
2018-03-10 23:19:40 +0530

    correcting the folder structure

--- sms_service/mainapp/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import url
-from . import api
-
-urlpatterns = [
-    url(r'^inbound/sms/$', api.InboundSMS.as_view()),
-    url(r'^outbound/sms/$', api.OutboundSMS.as_view())
-]
